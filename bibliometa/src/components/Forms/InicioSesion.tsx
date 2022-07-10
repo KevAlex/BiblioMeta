40ae61702a80ae0b5560be11e689b378d83168b1
KevAlex <kevin.alexdiaz@hotmail.com>
2022-07-10 21:55:57 +0200

    UI details

--- bibliometa/src/components/Forms/InicioSesion.tsx
+++ bibliometa/src/components/Forms/InicioSesion.tsx
@@ -34,18 +34,18 @@ function InicioSesion({ referencia }: VariableGlobal) {
       <div className="">
         <form className="" onSubmit={(e) => handleSubmit(e)}>
           <div className="d-flex flex-column justify-content-center align-items-center">
-            <div className="mt-2 ">
+            <div className="mt-2 h-100">
               <TextInput
-                classN=""
+                classN="h-75 flex-grow-1 p-2"
                 type="text"
                 label="Inserte su usuario"
                 onChange={handleChange}
                 name={"name"}
               />
             </div>
-            <div className="mt-2">
+            <div className="mt-2 mb-2">
               <TextInput
-                classN=""
+                classN="h-75 p-2"
                 type="password"
                 label="Inserte su contraseña"
                 onChange={handleChange}
@@ -54,7 +54,7 @@ function InicioSesion({ referencia }: VariableGlobal) {
             </div>
             <div>
               <a
-                className="App-link"
+                className="App-link "
                 href={referencia}
                 target="_blank"
                 rel="noopener noreferrer"
@@ -63,7 +63,7 @@ function InicioSesion({ referencia }: VariableGlobal) {
               </a>
             </div>
             <div className="mt-4">
-              <Button text="Iniciar sesion" type="submit" classN="" />
+              <Button text="Iniciar sesion" type="submit" classN="p-2" />
             </div>
           </div>
         </form>
