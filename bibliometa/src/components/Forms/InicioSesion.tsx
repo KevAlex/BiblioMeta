e0e23662a6e602c5da7fbf92051c4648e4a65d9c
KevAlex <kevin.alexdiaz@hotmail.com>
2022-07-10 21:32:58 +0200

    Centered position of UI set

--- bibliometa/src/components/Forms/InicioSesion.tsx
+++ bibliometa/src/components/Forms/InicioSesion.tsx
@@ -27,41 +27,48 @@ function InicioSesion({ referencia }: VariableGlobal) {
   };
 
   return (
-    <form className="" onSubmit={(e) => handleSubmit(e)}>
-      <div className="d-flex flex-column">
-        <div className="mt-2">
-          <TextInput
-            classN=""
-            type="text"
-            label="Inserte su usuario"
-            onChange={handleChange}
-            name={"name"}
-          />
-        </div>
-        <div className="mt-2">
-          <TextInput
-            classN=""
-            type="password"
-            label="Inserte su contraseña"
-            onChange={handleChange}
-            name={"password"}
-          />
-        </div>
-        <div>
-          <a
-            className="App-link"
-            href={referencia}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Olvidaste tu contraseña?
-          </a>
-        </div>
-        <div className="mt-4">
-          <Button text="Iniciar sesion" type="submit" classN="" />
-        </div>
+    <div className="d-flex flex-column mt-4 ">
+      <div>
+        <h1>Inicia Sesión en BiblioMeta</h1>
       </div>
-    </form>
+      <div className="">
+        <form className="" onSubmit={(e) => handleSubmit(e)}>
+          <div className="d-flex flex-column justify-content-center align-items-center">
+            <div className="mt-2 ">
+              <TextInput
+                classN=""
+                type="text"
+                label="Inserte su usuario"
+                onChange={handleChange}
+                name={"name"}
+              />
+            </div>
+            <div className="mt-2">
+              <TextInput
+                classN=""
+                type="password"
+                label="Inserte su contraseña"
+                onChange={handleChange}
+                name={"password"}
+              />
+            </div>
+            <div>
+              <a
+                className="App-link"
+                href={referencia}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                ¿Olvidaste tu contraseña?
+              </a>
+            </div>
+            <div className="mt-4">
+              <Button text="Iniciar sesion" type="submit" classN="" />
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
 
