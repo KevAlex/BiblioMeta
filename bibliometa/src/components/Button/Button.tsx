ba750176b7fe3ec794d086e6242846d79d6508d1
KevAlex <kevin.alexdiaz@hotmail.com>
2022-07-10 13:03:41 +0200

    class property to button added

--- bibliometa/src/components/Button/Button.tsx
+++ bibliometa/src/components/Button/Button.tsx
@@ -1,20 +1,17 @@
-import React, { SyntheticEvent } from 'react';
+import React, { SyntheticEvent } from "react";
 
 type ButtonProps = {
-	text: string;
-	type?: 'button' | 'submit';
-	onClick?: (e: SyntheticEvent) => void;
+  text: string;
+  type?: "button" | "submit";
+  classN: string;
+  onClick?: (e: SyntheticEvent) => void;
 };
 
-function Button({ onClick, text, type = 'button' }: ButtonProps) {
-	return (
-		<button
-			className="px-4 py-2 bg-blue-600 border border-blue-700 text-white hover:bg-blue-700 hover:border-blue-800 rounded-md"
-			onClick={onClick}
-			type={type}
-		>
-			{text}
-		</button>
-	);
+function Button({ onClick, text, type = "button", classN }: ButtonProps) {
+  return (
+    <button className={classN} onClick={onClick} type={type}>
+      {text}
+    </button>
+  );
 }
 export default Button;
