e0b809d78c6e73b79e1d8898cdfa8002ab8ac106
KevAlex <kevin.alexdiaz@hotmail.com>
2022-07-12 23:01:23 +0200

    Perfil de usuario UI mejorado

--- bibliometa/src/components/Usuario/PerfilUsuario.tsx
+++ bibliometa/src/components/Usuario/PerfilUsuario.tsx
@@ -1,10 +1,35 @@
 import React from "react";
-
+import Perfil from "../../images/Perfil.png";
 function PerfilUsuario() {
   return (
     <div className="d-flex flex-row">
       <div className="me-4">
-        <p>Panel izquierdo</p>
+        <div className="d-flex flex-col">
+          <div className="me-4">
+            <img src={Perfil} width="110" height="110"></img>
+          </div>
+          <div>
+            <h2>Hola, Brayan!</h2>
+            <p>Bienvenido a tu cuenta</p>
+          </div>
+        </div>
+        <div>
+          <div className="list-group">
+            <a
+              href="#"
+              className="list-group-item list-group-item-action active"
+              aria-current="true"
+            >
+              Mi cuenta
+            </a>
+            <a href="#" className="list-group-item list-group-item-action">
+              Historial de compras
+            </a>
+            <a href="#" className="list-group-item list-group-item-action">
+              Cerrar sesión
+            </a>
+          </div>
+        </div>
       </div>
 
       <div className="container">
@@ -12,7 +37,7 @@ function PerfilUsuario() {
           <h2>1. Información personal</h2>
         </div>
 
-        <div className="row">
+        <div className="row mb-4">
           <div className="col">
             <div className="form-floating">
               <input
@@ -22,27 +47,57 @@ function PerfilUsuario() {
               />
               <label> Correo</label>
             </div>
-            <p>Correo</p>
           </div>
           <div className="col">
-            <div></div>
-            <p>Fecha</p>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col">
-            <p>Nombre</p>
-          </div>
-          <div className="col">
-            <p>Apellido</p>
+            <div className="form-floating">
+              <input
+                className="form-control"
+                id="floatingInput"
+                value="12/05/1999"
+              />
+              <label>Fecha de nacimiento</label>
+            </div>
           </div>
         </div>
-        <div className="row">
+        <div className="row mb-4">
           <div className="col">
-            <p>Telefono fijo</p>
+            <div className="form-floating">
+              <input
+                className="form-control"
+                id="floatingInput"
+                value="Brayan Alonso"
+              />
+              <label>Nombre</label>
+            </div>
+          </div>
+
+          <div className="col">
+            <div className="form-floating">
+              <input
+                className="form-control"
+                id="floatingInput"
+                value="Pipa Hernandez"
+              />
+              <label>Apellido</label>
+            </div>
+          </div>
+        </div>
+        <div className="row mb-4  ">
+          <div className="col">
+            <div className="form-floating">
+              <input className="form-control" id="floatingInput" value="-" />
+              <label>Telefono</label>
+            </div>
           </div>
           <div className="col">
-            <p>Celular</p>
+            <div className="form-floating">
+              <input
+                className="form-control"
+                id="floatingInput"
+                value="+ 57 320445879"
+              />
+              <label>Celular</label>
+            </div>
           </div>
         </div>
         <div className="row">
@@ -50,10 +105,15 @@ function PerfilUsuario() {
         </div>
         <div className="row">
           <div className="col">
-            <p>Nueva contraseña</p>
-          </div>
-          <div className="col">
-            <p>Repetir contraseña</p>
+            <div className="form-floating">
+              <input
+                className="form-control"
+                id="floatingInput"
+                value="+ 57 320445879"
+                type="password"
+              />
+              <label>Contraseña</label>
+            </div>
           </div>
         </div>
       </div>
