46c28b86c03a39f655ac42ff4d1d41e458e573f6
KevAlex <kevin.alexdiaz@hotmail.com>
2022-08-15 13:56:27 +0200

    redirection to login set

--- bibliometa/src/components/GrillaLibros/GridLibros.jsx
+++ bibliometa/src/components/GrillaLibros/GridLibros.jsx
@@ -3,8 +3,15 @@ import libros from "../../informacion/libros.json";
 import styles from "./GridLibros.module.css";
 import { AppContext } from "../../services/ReferenceDataContext";
 import { useContext } from "react";
+import { Navigate, Redirect } from "react-router-dom";
 
 export function GridLibros() {
+  const [state, setState] = useContext(AppContext);
+
+  if (state.name === undefined) {
+    return <Navigate to="/login" />;
+  }
+
   return (
     <ul className={styles.gridLibros}>
       {libros.map((libro) => (
--- bibliometa/src/containers/GrillaLibrosContainer.jsx
+++ bibliometa/src/containers/GrillaLibrosContainer.jsx
@@ -3,11 +3,6 @@ import NavbarComp from "../components/NabvarM/NavbarComp";
 import Footer from "../components/Footer/footer";
 import Buscar from "../components/BuscarElemento/Buscar";
 import { useLocation } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { createBrowserHistory } from "history";
-import { AppContext } from "../services/ReferenceDataContext";
-import { useContext } from "react";
-import { Link } from "react-router-dom";
 
 function useQuery() {
   return new URLSearchParams(useLocation().search);
